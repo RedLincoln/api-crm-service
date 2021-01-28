506c3d318439fa3154ea5a32546a595117eba929
RedLincoln <miguel_lcp@hotmail.com>
2021-01-28 11:32:50 +0000

    Added basic seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "redlincoln", password: "password")
+User.create(username: "redlincoln", password_digest: "password")
