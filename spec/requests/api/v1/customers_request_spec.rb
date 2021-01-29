b79f26fb10c050bba040ac32a521dbb9e3560321
RedLincoln <miguel_lcp@hotmail.com>
2021-01-29 20:32:37 +0000

    added GET /customers initial state test

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Customers", type: :request do
 
+  describe 'initial state' do
+
+    context 'GET /customers' do
+      before{get customers_path}
+
+      it 'no customers' do
+        expect(json['customers']).to be_empty
+      end
+      
+      it 'status code' do
+        expect(response).to have_http_status(200)
+      end
+    end
+    
+  end
+
+
 end
