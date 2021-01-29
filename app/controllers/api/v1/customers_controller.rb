dc62cae5476b23c34b9b17856a3a9677ec577dac
RedLincoln <miguel_lcp@hotmail.com>
2021-01-29 16:22:00 +0000

    Link photo in customer controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::CustomersController < ApplicationController
 
   def index
-    render json: { customers: Customer.all }
+    render json: { customers: Customer.all.map { |customer| link_photo(customer)} }
   end
 
   def show
-    
+
   end
 
   def create
@@ -18,5 +18,14 @@ class Api::V1::CustomersController < ApplicationController
 
   def destroy
 
+  end
+
+
+  private
+
+  def link_photo(customer)
+    customer.as_json.tap do |hash|
+      hash['photo'] = customer.photo.attached? ? url_for(customer.photo) : nil
+    end
   end
 end
