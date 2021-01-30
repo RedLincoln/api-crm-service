a33b6723f9f0093ef388c2bae2632026623ee8f6
RedLincoln <miguel_lcp@hotmail.com>
2021-01-30 09:36:00 +0000

    Added test POST /login for bad login

--- spec/requests/api/v1/auth_request_spec.rb
+++ spec/requests/api/v1/auth_request_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Auths", type: :request do
   let(:password) { 'password' }
+  let(:bad_password) { 'password1' }
   let(:user) {create(:user, password: password)}
 
   describe 'login' do
@@ -22,5 +23,19 @@ RSpec.describe "Api::V1::Auths", type: :request do
       end
     end
 
+    context 'bad login' do
+
+      before {
+        post login_path, params: { username: user.username, password: bad_password }
+      }
+
+      it 'status code' do
+        expect(response).to have_http_status(401)
+      end
+
+      it 'content' do
+        expect(json).to include('error')
+      end
+    end
   end
 end
