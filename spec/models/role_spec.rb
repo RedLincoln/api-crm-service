40a990b586549f9f3d365fcddc5f7355aa3a672a
RedLincoln <miguel_lcp@hotmail.com>
2021-01-31 18:49:47 +0000

    Added validation test to model

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'validations' do
+
+    it { should validate_presence_of(:name) }
+    it { create(:role); should validate_uniqueness_of(:name) }
+  end
+
 end
