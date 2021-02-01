d3a0c17da2a7253eb56176fdf3566b772e548534
RedLincoln <miguel_lcp@hotmail.com>
2021-02-01 10:40:13 +0000

    Added test for GET /users/:id

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,18 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate
   
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { error: 'Not Found'}, status: :not_found
+  end
   
   def index
     @users = User.all
     render json: { users: @users }
   end
 
+
+  def show
+    @user = User.find(params[:id])
+    render json: { user: @user }
+  end
 end
--- spec/requests/api/v1/users_request/get_user_spec.rb
+++ spec/requests/api/v1/users_request/get_user_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users", type: :request do
+RSpec.describe "Api::V1::Users GET /users/:id", type: :request do
+  let(:total_users) { 3 }
+  let(:admin_role) { create(:role, name: 'admin')}
+  let(:admin) { create(:user, role: admin_role) }
+  let(:users) { create_list(:user, total_users - 1)}
+  let(:valid_user_id) { users.first.id }
 
+  context 'authenticated admin' do
+
+    context 'user exists' do
+
+      before {
+        get user_path(valid_user_id), headers: authorization_header(admin)
+      }
+
+      it 'returns the user requested' do
+        expect(json['user']['id']).to eq(valid_user_id)
+      end
+
+      it 'status code' do
+        expect(response).to have_http_status(:ok)
+      end
+
+    end
+
+    context 'user doest not exists' do
+
+      before {
+        not_valid_id = User.order('id').last.try(:id).to_i + 1
+        get user_path(not_valid_id), headers: authorization_header(admin)
+      }
+
+      it 'status code' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+    end
+  end
+  
+  context 'not authenticated admin' do
+
+    before {
+      get user_path(0)
+    }
+
+    it 'status code' do
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
 end
